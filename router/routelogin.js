a5c3fcb46252f0f6c5e6f1c6db2dbd7db6b98333
Anup mishra <40824863+Anupm0@users.noreply.github.com>
2025-02-12 17:20:06 +0530

    Update routelogin.js

--- router/routelogin.js
+++ router/routelogin.js
@@ -38,12 +38,9 @@ router.post('/signup', async (req, res) => {
         });
 
         if (existingUser) {
-            return res.status(400).json({
-                message: existingUser.email === formattedEmail
-                    ? 'Email already registered.'
-                    : 'Mobile number already registered.'
-            });
+            return res.status(400).json({ message: 'User already exists' });
         }
+
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const verificationToken = generateVerificationToken();
